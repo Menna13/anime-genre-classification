7fc218c4f0efb5d18088247bb45a53c98f589f56
mali1 <mali1>
2021-12-01 19:37:41 -0500

    balance

--- balance.py
+++ balance.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-f=pd.read_csv('animes.csv')
-print(f)
+df = pd.read_csv('animes.csv')
+df = df[['title', 'synopsis', 'genre']]
+df = df.dropna(subset=['genre'], how='all')
+#print(df['genre'])
+genres = set()
+for genre in df['genre']:
+    s = genre[1:-1].split(',')
+    for i in s:
+        stripped = i.replace('\'', '').replace(' ', '')
+        genres.add(stripped)
+print(len(genres), genres)
+
 
